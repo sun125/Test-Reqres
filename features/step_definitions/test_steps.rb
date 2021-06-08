e744ede4cff997914de1b591b543394d7ad6927f
sun125 <78115020+sun125@users.noreply.github.com>
2021-06-08 19:51:27 +0530

    Update test_steps.rb

--- features/step_definitions/test_steps.rb
+++ features/step_definitions/test_steps.rb
@@ -16,11 +16,3 @@ end
 Then(/^I should get the user details in the body$/) do
   p @api.body
 end
-
-And(/^I should get response message as "([^"]*)"$/) do |message|
-  @api.assert_equal(message, @api.message)
-end
-
-And(/^I should have response code as "([^"]*)"$/) do |code|
-  @api.assert_equal(code, @api.code)
-end
